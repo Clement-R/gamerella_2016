ee4ee3b5bdf326f65046cf5232fddab08d787a47
Clement-R <rondeau.clement@gmail.com>
2016-11-12 17:00:45 +0100

    Working on popup spawning and keeping track of them

--- Assets/Script/GameManager_PopupSpawner.cs
+++ Assets/Script/GameManager_PopupSpawner.cs
@@ -1,21 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager_PopupSpawner : MonoBehaviour {
 
     public float timeBetweenSpawn = 2.0f;
-
     public GameObject []popups;
+    public int activePopup = 0;
+
+    private List<GameObject> openPopups = new List<GameObject>();
 
 	void Start () {
         StartCoroutine("Spawn");
 	}
 	
     IEnumerator Spawn() {
+        // TODO : Choose random popup
         float width = popups[0].GetComponent<Renderer>().bounds.size.x;
         float height = popups[0].GetComponent<Renderer>().bounds.size.y;
 
-        Instantiate(popups[0], new Vector3(width / 2, - height / 2, -5), Quaternion.identity);
+        GameObject popup = Instantiate(popups[0], new Vector3(width / 2, -height / 2, -5), Quaternion.identity) as GameObject;
+        activePopup = popup.GetInstanceID();
+    Debug.Log(activePopup);
+        openPopups.Add(popup);
+
         yield return new WaitForSeconds(timeBetweenSpawn);
+        StartCoroutine("Spawn");
     }
 }
---
+++ Assets/Script/Popup_Behavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Popup_Behavior : MonoBehaviour {
+
+    private GameManager_PopupSpawner god;
+
+    void Start() {
+        god = GameObject.Find("GameManager").GetComponent<GameManager_PopupSpawner>();
+    }
+
+	void Update () {
+        Debug.Log("THIS : " + this.GetInstanceID());
+        Debug.Log("ACTIVE : " + god.activePopup);
+
+	    if(god.activePopup == this.GetInstanceID()) {
+            Debug.Log("I'm alive !");
+        }
+	}
+}
