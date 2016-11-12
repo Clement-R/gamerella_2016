a5ca86d70f14b7129089975c9e0393fad5fe5eb9
Clement-R <rondeau.clement@gmail.com>
2016-11-12 16:37:51 +0100

    Add popup spawner

---
+++ Assets/Script/GameManager_PopupSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager_PopupSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawn = 2.0f;
+
+    public GameObject []popups;
+
+	void Start () {
+        StartCoroutine("Spawn");
+	}
+	
+    IEnumerator Spawn() {
+        float width = popups[0].GetComponent<Renderer>().bounds.size.x;
+        float height = popups[0].GetComponent<Renderer>().bounds.size.y;
+
+        Instantiate(popups[0], new Vector3(width / 2, - height / 2, -5), Quaternion.identity);
+        yield return new WaitForSeconds(timeBetweenSpawn);
+    }
+}
